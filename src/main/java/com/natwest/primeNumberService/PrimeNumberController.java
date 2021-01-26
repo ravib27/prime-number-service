4a4753e9e4d3669be781684c2c3e96ef9110adb1
Ravi Bhuva <ravib27@gmail.com>
2021-01-26 23:05:51 +0100

    removed unwanted import

--- src/main/java/com/natwest/primeNumberService/PrimeNumberController.java
+++ src/main/java/com/natwest/primeNumberService/PrimeNumberController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
